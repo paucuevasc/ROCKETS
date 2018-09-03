8b4eb0d3402359b1f40c83cb753a18299099bd67
Pau Cuevas <pau.cuevas.c@hotmail.com>
2018-09-03 10:48:58 +0200

    Selection of rockets added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ this.name=name;
 this.nPropellers=nPropellers;
 }
 
-var rocket1=new Rocket("Enterprise",3);
-console.log(rocket1);
 
-rocketData=document.querySelector(".rocketData");
-rocketData.innerHTML="NAME: "+rocket1.name +"<br>"+ "nº propellers: " +rocket1.nPropellers;
+//var rocket1=new Rocket("Enterprise",3);
+//console.log(rocket1);
+
+function rocketCreator(){
+    var e = document.getElementById("rocketId");
+    var strRocket = e.options[e.selectedIndex].value;
+    if (strRocket==1) {
+        var rocket1=new Rocket("Enterprise",3);
+        console.log(rocket1);}
+    else if (strRocket==2) {
+        var rocket1=new Rocket("Tardis",6);
+        console.log(rocket1);    
+    }
+    rocketData=document.querySelector(".rocketData");
+    rocketData.innerHTML="NAME: "+rocket1.name +"<br>"+ "nº propellers: " +rocket1.nPropellers;
+}
