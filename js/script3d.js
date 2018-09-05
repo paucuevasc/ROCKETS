d3497afe3ce3764e1d56b10715e47d9081d0892f
Pau Cuevas <pau.cuevas.c@hotmail.com>
2018-09-05 12:06:46 +0200

    different accelerations added

--- js/script3d.js
+++ js/script3d.js
@@ -3,6 +3,7 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeigh
 var stars=[];
 var prevCubeC =0;
 var cubeC;
+var cube;
 var firstCoords=[null,null,null];
 
 var renderer = new THREE.WebGLRenderer();
@@ -40,83 +41,46 @@ function lineDrawer(){
 	firstCoords=coords;
 }
 controls=new THREE.OrbitControls(camera,renderer.domElement);
-//Create the shape
+function cubreCreator(cubeColor){
 var geometry=new THREE.BoxGeometry(cubeC,cubeC,cubeC);
-//var cubeMaterials=[
-//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide}),
-	//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide}),
-	//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide}),
-	//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide}),
-	//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide}),
-	//new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/image.png'), side:THREE.DoubleSide})
-//];
-//Create a material, colour or image texture
-var material = new THREE.MeshBasicMaterial({color:0x55FFFF,wireframe:false});
-//var material =new THREE.MeshFaceMaterial(cubeMaterials);
-var cube=new THREE.Mesh(geometry, material);
-scene.add(cube);
+var material = new THREE.MeshBasicMaterial({color:cubeColor,wireframe:false});
 
+cube=new THREE.Mesh(geometry, material);
+scene.add(cube);
+}
+
+geometry = new THREE.Geometry();
+for ( i = 0; i < 1000; i ++ ) {
+	var vertex = new THREE.Vector3();
+	vertex.x = Math.round((1000 * Math.random() - 500)/20)*20;
+	vertex.y = Math.round((1000 * Math.random() - 500)/20)*20;
+	vertex.z = Math.round((1000 * Math.random() - 500)/20)*20;
+	geometry.vertices.push( vertex );
+}
+material = new THREE.PointsMaterial( { size: 2, sizeAttenuation: false, transparent: true } );
+material.color.setHex(  );
+particles = new THREE.Points( geometry, material );
+particles.sortParticles = true;
+scene.add( particles );
 
 camera.position.z=10;
 
 var update = function(){
 	
-cube.rotation.x +=0.01;
-cube.rotation.y +=0.01;
-	
 
+if (isNaN(speed)==false){
+	
+	cube.translateZ(speed/1000 );
+
+}
 };
 
 
-function addSphere(){
-
-	// The loop will move from z position of -1000 to z position 1000, adding a random particle at each position. 
-	for ( var z= -1000; z < 1000; z+=20 ) {
-
-		// Make a sphere (exactly the same as before). 
-		var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
-		var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-		var sphere = new THREE.Mesh(geometry, material)
-
-		// This time we give the sphere random x and y positions between -500 and 500
-		sphere.position.x = Math.random() * 1000 - 500;
-		sphere.position.y = Math.random() * 1000 - 500;
-
-		// Then set the z position to where it is in the loop (distance of camera)
-		sphere.position.z = z;
-
-		// scale it up a bit
-		sphere.scale.x = sphere.scale.y = 2;
-
-		//add the sphere to the scene
-		scene.add( sphere );
-
-		//finally push it to the stars array 
-		stars.push(sphere); 
-	}
-}
-
-function animateStars() { 
-	
-// loop through each star
-for(var i=0; i<stars.length; i++) {
-
-star = stars[i]; 
-	
-// and move it forward dependent on the mouseY position. 
-star.position.z +=  i/10;
-	
-// if the particle is too close move it to the back
-if(star.position.z>1000) star.position.z-=2000; 
-
-}
-
-}
 
 
 var render = function(){
 renderer.render(scene,camera);
-animateStars();
+
 };
 
 
@@ -124,7 +88,39 @@ var GameLoop=function(){
 requestAnimationFrame(GameLoop);
 update();
 render();
-addSphere();
+
 };
 
 GameLoop();
+
+
+
+var color = [100, 100, 200];
+function draw() 
+	{
+	camera.position.x = Math.sin( Date.now() * 0.003 ) * 10 
+										+ Math.cos( Date.now() * 0.003 ) * 10;
+	camera.position.y = Math.cos( Date.now() * 0.006 ) * 10;
+	camera.rotation.z = Math.cos( Date.now() * 0.00006 ) * 50 +500;
+	color = color.map(function(d, i){ return (d + 1 + i) % 256 })
+	
+	geometry = new THREE.Geometry();
+	material.color.set( new THREE.Color(color[2], color[2], color[2]))
+	material.color.set( new THREE.Color('white'))
+	
+	for ( i = 0; i < 60; i ++ ) {
+		var vertex = new THREE.Vector3();
+		vertex.x = Math.round((1000 * Math.random() - 500)/20)*20;
+		vertex.y = Math.round((1000 * Math.random() - 500)/20)*20;
+		vertex.z = Math.round((1000 * Math.random() - 500)/20)*20;
+		geometry.vertices.push( vertex );
+	}
+	
+	particles = new THREE.Points( geometry, material );
+	particles.sortParticles = true;
+	scene.add( particles );
+}
+	requestAnimationFrame( draw );
+
+	setup();
+	draw();
